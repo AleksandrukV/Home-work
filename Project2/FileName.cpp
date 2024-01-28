7e02dc9f0927d4f52943a00845f6865e97f36329
AleksandrukV <valentynaleksandruk2005@gmail.com>
2024-01-28 19:19:44 +0200

    Add function PrintArr

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -21,6 +21,15 @@ void Delete(int* arr)
 	delete[] arr;
 }
 
+void PrintArr(int* arr, int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
 int main()
 {
 	int size = 10;
